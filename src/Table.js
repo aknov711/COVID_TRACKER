f31b5aee670486cb2fcb3d3ef2bde8ea135dd984
Anand Kumar <61695228+aknov711@users.noreply.github.com>
2021-06-26 17:28:23 +0530

    Added some more features

--- src/Table.js
+++ src/Table.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import './table.css'
-function Table({countries}) {
-    return (
-        <div classsName="table"  style={{marginTop:'20px',height:'400px',overflow:'scroll'}}>
-           {
-             countries.map(({country,cases})=>(
-                 <tr style={{display:'flex',justifyContent:'space-between',padding:'7px'}}>
-                     <td>{country}</td>
-                     <td>{cases}</td>
-                 </tr>
-             ))
-           }
-        </div>
-    )
+import React from "react";
+import "./table.css";
+import numeral from "numeral";
+
+function Table({ countries }) {
+  return (
+    <div className="table">
+      {countries.map((country) => (
+        <tr>
+          <td>{country.country}</td>
+          <td>
+            <strong>{numeral(country.cases).format("0,0")}</strong>
+          </td>
+        </tr>
+      ))}
+    </div>
+  );
 }
 
-export default Table
+export default Table;
